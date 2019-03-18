e6fa7d176534d6ae232415b3a9259ee6bdb1a4df
zhangyuhua583 <1457896625@qq.com>
2019-03-18 18:00:40 +0800

    quit and scope

---
+++ buildin.py
@@ -0,0 +1,7 @@
+#coding uuf-8 
+
+print __file__ 
+print __name__  
+print __doc__  
+print __package__  
+print __builtins__  
--- modules/module.py
+++ modules/module.py
@@ -1,9 +1,10 @@
+# coding:utf-8
 # 1 import 整个模块 
-#import hua_print 
+import hua_print 
 
 #2 按需引入
-from hua_print import hprint
-hprint("wahaha")
+# from hua_print import hprint
+# hprint("wahaha")
 
 '''
 1、当前目录
@@ -11,3 +12,19 @@ hprint("wahaha")
 3、如果都找不到，Python会察看默认路径。UNIX下，默认路径一般为/usr/local/lib/python/
 
 '''
+
+'''
+作用域和命名空间  
+
+'''
+money = 1;
+
+def say():
+    money = 2
+    print money 
+say()
+
+'''
+dir函数返回 模块里定义的所有模块
+'''
+print dir(hua_print)
