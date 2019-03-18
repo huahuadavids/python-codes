7019ae64392ae847a2792052319ef8f53fd082ca
zhangyuhua583 <1457896625@qq.com>
2019-03-18 17:40:54 +0800

    modules

--- modules/module.py
+++ modules/module.py
@@ -1,3 +1,13 @@
-import hua_print 
+# 1 import 整个模块 
+#import hua_print 
 
-hua_print.hprint("wahaha")
+#2 按需引入
+from hua_print import hprint
+hprint("wahaha")
+
+'''
+1、当前目录
+2、如果不在当前目录，Python 则搜索在 shell 变量 PYTHONPATH 下的每个目录。set PYTHONPATH=/usr/local/lib/python
+3、如果都找不到，Python会察看默认路径。UNIX下，默认路径一般为/usr/local/lib/python/
+
+'''
